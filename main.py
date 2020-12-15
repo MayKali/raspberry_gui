72aa05646cdfda316e5f2d9bfa669f6b05a2922f
Mohamad Abbas <40679821+MayKali@users.noreply.github.com>
2020-12-15 11:01:32 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,80 +1,166 @@
+import matplotlib
+matplotlib.use("TkAgg")
+import numpy as np 
 
-#import os
-#import time
-from datetime import datetime
+import tkinter as tk
+from tkinter.ttk import *
+from tkinter import *
+
 import math
+import datetime as dt
 import time
-import random 
-#import Adafruit_ADS1x15
-#import csv
+from collections import Counter
+import random as rn
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
+import matplotlib.animation as animation
+from matplotlib import style
+import matplotlib.pyplot as plt
 
 
-# Choose a gain of 1 for reading voltages from 0 to 4.09V.
-# Or pick a different gain to change the range of voltages that are read:
-#  - 2/3 = +/-6.144V
-#  -   1 = +/-4.096V
-#  -   2 = +/-2.048V
-#  -   4 = +/-1.024V
-#  -   8 = +/-0.512V
-#  -  16 = +/-0.256V
-# See table 3 in the ADS1015/ADS1115 datasheet for more info on gain.
+from multiprocessing import Process
 
-#adc = Adafruit_ADS1x15.ADS1115()
-#GAIN = 2/3
+TOT = []
+CPM = 0
+temp = 0
 
-#The division of the two has to be a whole number 
+def runGraph():
+    # Parameters
 
-interval = 60
-speed = 0.3
-sleepTime = int(interval / speed)
+    x_len = 50         # Number of points to display
+    y_range = [0, 40]  # Range of possible Y values to display
 
-print('Reading ADS1x15 values, press Ctrl-C to quit...')
-# Print nice channel column headers.
-print('| {0:>6} |'.format(*range(1)))
-print('-' * (27))
+    # Create figure for plotting
+    fig1, ax1 = plt.subplots()
+    fig2, ax2 = plt.subplots()
+    x1 = list(range(0, 50))
+    x2 = list(range(0, 50))
+    y1 = [0] * x_len
+    y2 = [0] * x_len
 
-# Main loop
-#f = open('/home/pi/pressure.txt', 'r+')
+    ax1.set_ylim(y_range)
+    ax2.set_ylim(y_range)
 
-while 1:
+    canvas1 = FigureCanvasTkAgg(fig1, master=root)
+    canvas1.get_tk_widget().grid(row = 2, column = 0)
 
-    f = open('test.txt', 'r+')
-    f.truncate(0)
-    num = 0
-    j = 0
+    canvas2 = FigureCanvasTkAgg(fig2, master=root)
+    canvas2.get_tk_widget().grid(row = 2, column = 2)
 
-    for i in range(0,sleepTime):
-        with open('test.txt', 'a') as f:
-
-            #value = adc.read_adc_difference(0, gain=GAIN)
-            
-            #Voltage is calculated from the 15 bit returned value by dividing by 32,767 for 10 bit res
-            #and multiplied by 6.144 for the maximum gain voltage
-            #volts = value *((3*(6.144)/32768))
-            
-            #Relationship between volt and pressure in a cold cathode gauge 
-            #Pa = math.exp((volts-10)/1.33)
-            
-            #Record the time  the data was taken
-            #recordTime = (time.strftime("%M:%S"))
-            
-            #Write the data into the .txt file
-            #f.write(recordTime + ", "+ str(volts[0])+"\n")
-            num = random.uniform(4,10)
-            
-            f.write(("{0}, {1} \n".format(j,num)))
-
-            j += speed
-            
-            #Check if the device is recording values
-            #print('Channel 0 minus 1: {}, {}'.format(value, volts))
-            
-            
-            time.sleep(speed)  
-    
+    # Create a blank line. We will update the line in animate
+    line1, = ax1.plot(x1, y1)
+    line2, = ax2.plot(x2, y2)
 
 
+    # Add labels
+    # plt.title('TMP102 Temperature over Time')
+    # plt.xlabel('Samples')
+    # plt.ylabel('Temperature (deg C)')
 
+    # This function is called periodically from FuncAnimation
+    def animate(i, ys):
+
+        # Read temperature (Celsius) from TMP102
+        temp_c = np.random.random(1)*40
+
+        # Add y to list
+        ys.append(temp_c)
+
+        # Limit y list to set number of items
+        ys = ys[-x_len:]
+
+        # Update line with new Y values
+        line1.set_ydata(ys)
+
+        return line1,
+
+    def animate2(i, ys):
+
+        # Read temperature (Celsius) from TMP102
+        temp_b = np.random.random(1)*40
+
+        # Add y to list
+        ys.append(temp_b)
+
+        # Limit y list to set number of items
+        ys = ys[-x_len:]
+
+        # Update line with new Y values
+        line2.set_ydata(ys)
+
+        return line2,
+
+
+    # Set up plot to call animate() function periodically
+
+    ani = animation.FuncAnimation(fig1,
+        animate,
+        fargs=(y1,),
+        interval=200,
+        blit=True)
+
+    ani2 = animation.FuncAnimation(fig2,
+        animate2,
+        fargs=(y2,),
+        interval=200,
+        blit=True)
+    plt.show()
+
+
+def time():
+
+	string = dt.datetime.now().strftime('%H:%M:%S %p') 
+	clock.config(text = string)
+	clock.after(1000, time)
+
+
+def MainProgram():
+
+	global TOT, CPM, lbl
+
+
+	while 1:
+
+		temp = rn.randint(0,1)
+
+		if temp == True:
+			TOT.append(True)
+		else:
+			TOT.append(False)
+
+		TOT = TOT[-2750:]
+		count = Counter(TOT)
+		CPM = count[True]
+
+		lbl.config(text = "CPM = {}".format(CPM)) 	
+
+		print(dt.datetime.now().strftime('%H: %M: %S.%f'))
+		lbl.after(500, MainProgram)
+
+root = tk.Tk()
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+
+lbl = ttk.Label(root, font = ('Sans Serif', 40, 'bold'), 
+background = 'purple', 
+foreground = 'White')
+lbl.grid(row=3, column = 1)
+
+clock = ttk.Label(root, font = ('Sans Serif', 40, 'bold'), 
+background = 'purple', 
+foreground = 'White') 
+clock.grid(row=1, column = 1)
+
+if __name__ == '__main__':
+
+	p = Process(target=runGraph)
+	p.start()
+	MainProgram()
+	time()
+	p.join()
+	root.mainloop()
 
 
 
